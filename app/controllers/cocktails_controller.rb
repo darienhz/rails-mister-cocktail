162a500d48e132abd13b2dfc61e935b928cca176
Darien Hernandez <darienhz89@gmail.com>
2020-11-13 15:38:42 -0600

    fixed the photo issue

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -14,7 +14,6 @@ class CocktailsController < ApplicationController
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
-    @cocktail.save
     if @cocktail.save
       redirect_to cocktail_path(@cocktail)
     else
@@ -25,6 +24,6 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
